99dc3eefa1031c401b6674e2815bfc87f4f4b986
Pupov Ivan <117669912+ciklon18@users.noreply.github.com>
2025-12-19 00:16:51 +0700

    Set up Spring app Docker Compose with Postgres env vars

--- node-app/db.js
+++ node-app/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'mysql',
-    password: 'mysql',
-    database: 'db'
+    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER || 'mysql',
+    password: process.env.MYSQL_PASSWORD || 'mysql',
+    database: process.env.MYSQL_DATABASE || 'db'
 });
 
 module.exports = { connection };
